be8a63a89dbe1087992ac735c33e92d87e430a6d
Eduardo Faria <115400010+2101dudu@users.noreply.github.com>
2025-02-17 21:55:25 +0000

    feat: plane gen (#5)
    * fix: gitignore
    
    * feat: first iteration of a box coordinate system
    
    * feat: added triangular associations
    
    * fix: minor optimization
    
    * Update box.cpp
    
    * fix: rename box to plane
    
    * fix: remove hardcoded variables
    
    ---------
    
    Co-authored-by: Nuno Silva <135663404+NunoMRS7@users.noreply.github.com>

--- generator/src/main.cpp
+++ generator/src/main.cpp
@@ -1,21 +1,23 @@
 #include "PointsGenerator.hpp"
 #include "FileWriter.hpp"
+#include "plane.hpp"
 
 int main() {
     PointsGenerator generator;
 
-    generator.addPoint(0.0, 1.0, 0.0);
-    generator.addPoint(-1.0, -1.0, 1.0);
-    generator.addPoint(1.0, -1.0, 1.0);
-    generator.addPoint(-1.0, -1.0, -1.0);
-    generator.addPoint(1.0, -1.0, -1.0);
+    //generator.addPoint(0.0, 1.0, 0.0);
+    //generator.addPoint(-1.0, -1.0, 1.0);
+    //generator.addPoint(1.0, -1.0, 1.0);
+    //generator.addPoint(-1.0, -1.0, -1.0);
+    //generator.addPoint(1.0, -1.0, -1.0);
 
-    generator.addAssociation(1, 2, 3);
-    generator.addAssociation(2, 4, 5);
+    //generator.addAssociation(1, 2, 3);
+    //generator.addAssociation(2, 4, 5);
 
-    generator.showData();
+    Plane::createPlane(2, 3);
 
-    FileWriter::writeToFile("pontos.txt", generator);
+
+    //FileWriter::writeToFile("pontos.txt", generator);
 
     return 0;
 }
