a23ef6a6cf64ebf530597340574460f55fd67349
Eduardo Faria <115400010+2101dudu@users.noreply.github.com>
2025-02-18 18:30:00 +0000

    fix: CMakeLists to properly work on macos (#7)
    * fix: CMakeLists to properly work on macos
    
    * chore: adapt to various os
    
    * fix: OS specific typecast to only target macOS
    
    ---------
    
    Co-authored-by: Pedro Pereira <pfpereira2004@gmail.com>

--- generator/src/PointsGenerator.cpp
+++ generator/src/PointsGenerator.cpp
@@ -1,11 +1,17 @@
 #include "PointsGenerator.hpp"
 
+#ifdef __APPLE__
+#define CAST_POINT (Point)
+#else
+#define CAST_POINT
+#endif
+
 void PointsGenerator::addPoint(float x, float y, float z) {
-    points.push_back((Point) {x, y ,z});
+    points.push_back(CAST_POINT {x, y ,z});
 }
 
 void PointsGenerator::addAssociation(int p1, int p2, int p3) {
-    associations.push_back((Association) {p1, p2, p3});
+    associations.push_back(CAST_POINT {p1, p2, p3});
 }
 
 void PointsGenerator::showData() const {
--- generator/src/main.cpp
+++ generator/src/main.cpp
@@ -1,23 +1,20 @@
-#include "PointsGenerator.hpp"
 #include "FileWriter.hpp"
+#include "PointsGenerator.hpp"
 #include "plane.hpp"
 
 int main() {
-    PointsGenerator generator;
+  PointsGenerator generator;
 
-    //generator.addPoint(0.0, 1.0, 0.0);
-    //generator.addPoint(-1.0, -1.0, 1.0);
-    //generator.addPoint(1.0, -1.0, 1.0);
-    //generator.addPoint(-1.0, -1.0, -1.0);
-    //generator.addPoint(1.0, -1.0, -1.0);
+  // generator.addPoint(0.0, 1.0, 0.0);
+  // generator.addPoint(-1.0, -1.0, 1.0);
+  // generator.addPoint(1.0, -1.0, 1.0);
+  // generator.addPoint(-1.0, -1.0, -1.0);
+  // generator.addPoint(1.0, -1.0, -1.0);
 
-    //generator.addAssociation(1, 2, 3);
-    //generator.addAssociation(2, 4, 5);
+  // generator.addAssociation(1, 2, 3);
+  // generator.addAssociation(2, 4, 5);
 
-    Plane::createPlane(2, 3);
+  Plane::createPlane(2, 3);
 
-
-    //FileWriter::writeToFile("pontos.txt", generator);
-
-    return 0;
+  return 0;
 }
