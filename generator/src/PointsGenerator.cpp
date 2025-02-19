c97b11ee5fdd76d9b9454111c795bfa8ba7b1448
Eduardo Faria <eduardo21faria@gmail.com>
2025-02-19 11:10:49 +0000

    fix: macos cast for associations

--- generator/src/PointsGenerator.cpp
+++ generator/src/PointsGenerator.cpp
@@ -2,19 +2,24 @@
 
 #ifdef __APPLE__
 #define CAST_POINT (Point)
+#define CAST_ASSOCIATION (Association)
 #else
 #define CAST_POINT
+#define CAST_ASSOCIATION
 #endif
 
-void PointsGenerator::addPoint(float x, float y, float z) {
-    points.push_back(CAST_POINT {x, y ,z});
+void PointsGenerator::addPoint(float x, float y, float z)
+{
+    points.push_back(CAST_POINT { x, y, z });
 }
 
-void PointsGenerator::addAssociation(int p1, int p2, int p3) {
-    associations.push_back(CAST_POINT {p1, p2, p3});
+void PointsGenerator::addAssociation(int p1, int p2, int p3)
+{
+    associations.push_back(CAST_ASSOCIATION { p1, p2, p3 });
 }
 
-void PointsGenerator::showData() const {
+void PointsGenerator::showData() const
+{
     std::cout << points.size() << "\n";
     for (const auto& p : points) {
         std::cout << p.x << " " << p.y << " " << p.z << "\n";
