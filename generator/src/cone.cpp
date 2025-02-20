1fbdfddc3d9ffd7a18a96e20419e9b63f7c789cc
Eduardo Faria <eduardo21faria@gmail.com>
2025-02-20 15:24:22 +0000

    fix: slice count and base's associations

--- generator/src/cone.cpp
+++ generator/src/cone.cpp
@@ -3,7 +3,8 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-void Cone::createCone(float radius, float height, int slices, int stacks) {
+void Cone::createCone(float radius, float height, int slices, int stacks)
+{
     PointsGenerator generator;
     float stackHeight = height / stacks;
     float angleStep = 2 * M_PI / slices;
@@ -32,20 +33,16 @@ void Cone::createCone(float radius, float height, int slices, int stacks) {
         }
     }
 
-    int tipIndex = generator.points.size();
-    generator.addPoint(0, height, 0);
-    for (int j = 0; j < slices; ++j) {
-        int p1 = (stacks - 1) * (slices + 1) + j;
-        int p2 = p1 + 1;
-        generator.addAssociation(p1, p2, tipIndex);
-    }
-
-    int baseCenterIndex = generator.points.size();
     generator.addPoint(0, 0, 0);
-    for (int j = 0; j < slices; ++j) {
-        int p1 = j;
-        int p2 = (j + 1) % slices;
-        generator.addAssociation(p1, baseCenterIndex, p2);
+    int baseCenterIndex = generator.points.size();
+
+    // if we have 9 slices, i.e., 9 points, we need to
+    // have N + 1 associations
+    for (int j = 0; j <= slices; j++) {
+        int p1 = j + 1; // an index, for now, is never 0
+        int p2 = (j + 2) % (slices + 1); // look for the neighbor point, but ensure it doesn't overflow
+        p2 += p2 == 0 ? 1 : 0; // if a points overflows, the result will be the index 0, which, for now, cannot happen
+        generator.addAssociation(p1, p2, baseCenterIndex);
     }
 
     FileWriter::writeToFile("pontos.txt", generator);
