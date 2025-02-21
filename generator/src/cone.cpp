6bb3f363696dfe6e92b666c3e5847c08dbc43dab
Eduardo Faria <eduardo21faria@gmail.com>
2025-02-21 10:01:24 +0000

    fix: optimized cone generation

--- generator/src/cone.cpp
+++ generator/src/cone.cpp
@@ -12,24 +12,28 @@ void Cone::createCone(float radius, float height, int slices, int stacks)
     for (int i = 0; i <= stacks; ++i) {
         float currentHeight = i * stackHeight;
         float currentRadius = radius * (1 - (float)i / stacks);
-        for (int j = 0; j <= slices; ++j) {
+        for (int j = 0; j < slices; ++j) {
             float angle = j * angleStep;
             float x = currentRadius * cos(angle);
             float y = currentHeight;
             float z = currentRadius * sin(angle);
             generator.addPoint(x, y, z);
-        }
-    }
 
-    for (int i = 0; i < stacks; ++i) {
-        for (int j = 0; j <= slices; ++j) {
-            int p1 = i * (slices + 1) + j;
-            int p2 = p1 + 1;
-            int p3 = p1 + (slices + 1);
-            int p4 = p3 + 1;
+            int currPointIndex = i * slices + (j + 1);
+            int neighbourPointIndex = (currPointIndex + 1) % (slices * (i + 1) + 1);
+            neighbourPointIndex += neighbourPointIndex == 0 ? 1 + slices * i : 0;
 
-            generator.addAssociation(p1, p3, p2);
-            generator.addAssociation(p2, p3, p4);
+            int p1 = currPointIndex;
+            int p2 = neighbourPointIndex;
+            int p3 = neighbourPointIndex + slices;
+            int p4 = currPointIndex + slices;
+
+            if (i < stacks) {
+                generator.addAssociation(p1, p3, p2);
+                generator.addAssociation(p1, p4, p3);
+            } else {
+                generator.addAssociation(p1, p4, p2);
+            }
         }
     }
 
@@ -37,8 +41,8 @@ void Cone::createCone(float radius, float height, int slices, int stacks)
     int baseCenterIndex = generator.points.size();
 
     // if we have 9 slices, i.e., 9 points, we need to
-    // have N + 1 associations
-    for (int j = 0; j <= slices; j++) {
+    // have N associations
+    for (int j = 0; j < slices; j++) {
         int p1 = j + 1; // an index, for now, is never 0
         int p2 = (j + 2) % (slices + 1); // look for the neighbor point, but ensure it doesn't overflow
         p2 += p2 == 0 ? 1 : 0; // if a points overflows, the result will be the index 0, which, for now, cannot happen
--- generator/src/main.cpp
+++ generator/src/main.cpp
@@ -1,23 +1,24 @@
 #include "FileWriter.hpp"
-#include "box.hpp"
 #include "PointsGenerator.hpp"
+#include "box.hpp"
+#include "cone.hpp"
 #include "plane.hpp"
 #include "sphere.hpp"
-#include "cone.hpp"
 
-int main() {
-  PointsGenerator generator;
+int main()
+{
+    PointsGenerator generator;
 
-    //generator.showData();
+    // generator.showData();
 
     Box::createBox(2, 1);
-    //Plane::createPlane(2, 3);
+    // Plane::createPlane(2, 3);
 
-    //Plane::createPlane(2, 3);
+    // Plane::createPlane(2, 3);
     Sphere::createSphere(1, 10, 10);
 
     // Example call to create a cone
-    Cone::createCone(1.0f, 2.0f, 40, 3);
+    Cone::createCone(1.0f, 2.0f, 4, 3);
 
-  return 0;
+    return 0;
 }
