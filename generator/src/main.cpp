e8dd1e85a295ecb201d7235e84ffb28f4b8a53bd
Eduardo Faria <eduardo21faria@gmail.com>
2025-02-21 16:29:37 +0000

    fix: added option for new primitive types

--- generator/src/main.cpp
+++ generator/src/main.cpp
@@ -4,25 +4,25 @@
 #include "plane.hpp"
 #include "sphere.hpp"
 
-void executeCommand(const std::string& command, const std::vector<int>& args) {
+void executeCommand(const std::string& command, const std::vector<int>& args)
+{
     if (command == "sphere" && args.size() == 3) {
         Sphere::createSphere(args[0], args[1], args[2]);
-    }
-    else if (command == "box" && args.size() == 2) {
+    } else if (command == "box" && args.size() == 2) {
         Box::createBox(args[0], args[1]);
-    }
-    else if (command == "plane" && args.size() == 2) {
+    } else if (command == "plane" && args.size() == 2) {
         Plane::createPlane(args[0], args[1]);
-    }
-    else if (command == "cone") {
-        std::cout << "Cone generation is not yet implemented." << std::endl;
-    }
-    else {
+    } else if (command == "cone" && args.size() == 4) {
+        Plane::createCone(args[0], args[1], args[2], args[3]);
+    } else if (command == "torus" && args.size() == 4) {
+        Torus::createTorus(args[0], args[1], args[2], args[3]);
+    } else {
         std::cerr << "Unknown command or incorrect parameters: " << command << std::endl;
     }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     // minimum: "generator command param1 param2 ..."
     if (argc < 5) {
         std::cerr << "Usage: generator <command> <param1> <param2> ...\n";
@@ -34,7 +34,7 @@ int main(int argc, char* argv[]) {
     std::vector<int> args;
 
     // convert parameters to integers
-    for (int i = 2; i < argc-1; ++i) {
+    for (int i = 2; i < argc - 1; ++i) {
         args.push_back(std::stoi(argv[i]));
     }
 
