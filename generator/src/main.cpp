6615fc0f6ac81af644c029578c2587aeece4b81f
Eduardo Faria <115400010+2101dudu@users.noreply.github.com>
2025-02-22 21:56:41 +0000

    feat: torus primitve implemented (#16)

--- generator/src/main.cpp
+++ generator/src/main.cpp
@@ -1,20 +1,22 @@
 #include "FileWriter.hpp"
-#include "box.hpp"
 #include "PointsGenerator.hpp"
+#include "box.hpp"
 #include "plane.hpp"
 #include "sphere.hpp"
+#include "torus.hpp"
 
-int main() {
-  PointsGenerator generator;
+int main()
+{
+    PointsGenerator generator;
 
-    //generator.showData();
+    // generator.showData();
 
-    Box::createBox(2, 1);
-    //Plane::createPlane(2, 3);
+    // Box::createBox(2, 1);
+    //  Plane::createPlane(2, 3);
 
-  //Plane::createPlane(2, 3);
-  Sphere::createSphere(1, 10, 10);
+    // Plane::createPlane(2, 3);
+    // Sphere::createSphere(1, 10, 10);
+    Torus::createTorus(1.5, 0.6, 40, 15);
 
-
-  return 0;
+    return 0;
 }
---
+++ generator/src/torus.cpp
@@ -0,0 +1,45 @@
+//
+// Created by Eduardo Faria on 21/02/2025.
+//
+
+#include "torus.hpp"
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+void Torus::createTorus(float radius, float minorRadius, int slices, int stacks)
+{
+    PointsGenerator pointGen;
+
+    // angle per slice
+    float alpha = 2 * M_PI / slices;
+
+    // angle per stack
+    float beta = 2 * M_PI / stacks;
+
+    for (int i = 0; i <= slices; i++) {
+        float currAlpha = alpha * i;
+
+        for (int j = 0; j <= stacks; j++) {
+            float currBeta = beta * j;
+
+            float x = (radius + minorRadius * cosf(currBeta)) * cosf(currAlpha);
+            float y = minorRadius * sinf(currBeta);
+            float z = (radius + minorRadius * cosf(currBeta)) * sinf(currAlpha);
+
+            pointGen.addPoint(x, y, z);
+
+            int currPointIndex = (stacks + 1) * i + j + 1;
+
+            int neighbourIndex = (currPointIndex + stacks + 1) % ((slices + 1) * (stacks + 1));
+
+            if (j < stacks) {
+                pointGen.addAssociation(currPointIndex, currPointIndex + 1, neighbourIndex);
+                pointGen.addAssociation(neighbourIndex, currPointIndex + 1, neighbourIndex + 1);
+            }
+        }
+    }
+
+    pointGen.showData();
+
+    FileWriter::writeToFile("pontos.txt", pointGen);
+}
