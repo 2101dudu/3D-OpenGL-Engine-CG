265ea1e4f642c6bd7c5b75cf6cb78315df54bf07
Eduardo Faria <115400010+2101dudu@users.noreply.github.com>
2025-02-24 18:19:28 +0000

    fix: ensure type cast to float to center plane generation (#25)

--- generator/src/plane.cpp
+++ generator/src/plane.cpp
@@ -4,40 +4,41 @@
 
 #include "plane.hpp"
 
-void Plane::createPlane(int dimension, int divPerEdge) {
-  PointsGenerator pointGen;
+void Plane::createPlane(int dimension, int divPerEdge)
+{
+    PointsGenerator pointGen;
 
-  // the interval specified by the dimension, i.e.,
-  // dimension = 2 --> [-1,1]
-  const float maxWidth = dimension / 2;
+    // the interval specified by the dimension, i.e.,
+    // dimension = 2 --> [-1,1]
+    const float maxWidth = static_cast<float>(dimension) / 2;
 
-  // the number of unique points per number of divisions,
-  // i.e., 3 divisions means 4 points
-  const int pointsPerEdge = divPerEdge + 1;
+    // the number of unique points per number of divisions,
+    // i.e., 3 divisions means 4 points
+    const int pointsPerEdge = divPerEdge + 1;
 
-  for (int i = 0; i < pointsPerEdge; i++) {
-    for (int j = 0; j < pointsPerEdge; j++) {
-      const float x = maxWidth - 2.0f * i / divPerEdge;
-      const float y = 0.0f;
-      const float z = maxWidth - 2.0f * j / divPerEdge;
-      pointGen.addPoint(x, y, z);
+    for (int i = 0; i < pointsPerEdge; i++) {
+        for (int j = 0; j < pointsPerEdge; j++) {
+            const float x = maxWidth - static_cast<float>(dimension) * static_cast<float>(i) / static_cast<float>(divPerEdge);
+            const float y = 0.0f;
+            const float z = maxWidth - static_cast<float>(dimension) * static_cast<float>(j) / static_cast<float>(divPerEdge);
+            pointGen.addPoint(x, y, z);
 
-      int currPointIndex = pointsPerEdge * i + (j + 1);
+            int currPointIndex = pointsPerEdge * i + (j + 1);
 
-      // north-facing triangles
-      if (j < divPerEdge && i < divPerEdge) {
-        pointGen.addAssociation(currPointIndex, currPointIndex + 1,
-                                currPointIndex + pointsPerEdge);
-      }
-      // south-facing triangles
-      if (j > 0 && i > 0) {
-        pointGen.addAssociation(currPointIndex, currPointIndex - 1,
-                                currPointIndex - pointsPerEdge);
-      }
+            // north-facing triangles
+            if (j < divPerEdge && i < divPerEdge) {
+                pointGen.addAssociation(currPointIndex, currPointIndex + 1,
+                    currPointIndex + pointsPerEdge);
+            }
+            // south-facing triangles
+            if (j > 0 && i > 0) {
+                pointGen.addAssociation(currPointIndex, currPointIndex - 1,
+                    currPointIndex - pointsPerEdge);
+            }
+        }
     }
-  }
 
-  pointGen.showData();
+    pointGen.showData();
 
-  FileWriter::writeToFile("pontos.txt", pointGen);
+    FileWriter::writeToFile("pontos.txt", pointGen);
 }
