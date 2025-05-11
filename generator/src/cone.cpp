9ec023c7d0ccceb2041021fb89d026f83daa4886
dudu <eduardo21faria@gmail.com>
2025-05-11 16:06:33 +0100

    fix: top and bottom normals for cone

--- generator/src/cone.cpp
+++ generator/src/cone.cpp
@@ -9,17 +9,22 @@ void Cone::createCone(float radius, float height, int slices, int stacks, const 
     float stackHeight = height / stacks;
     float angleStep = 2 * M_PI / slices;
 
-    for (int i = 0; i <= stacks; ++i) {
-        float currentHeight = i * stackHeight;
-        float currentRadius = radius * (1 - (float)i / stacks);
+    int offset = 0;
+    for (int i = -1; i <= stacks; ++i) {
+        int index = std::max(i, 0);
+        float currentHeight = index * stackHeight;
+        float currentRadius = radius * (1 - (float)index / stacks);
         for (int j = 0; j < slices; ++j) {
             float angle = j * angleStep;
             float x = currentRadius * cos(angle);
             float y = currentHeight;
             float z = currentRadius * sin(angle);
 
-            float nx = cos(angle);
-            float nz = sin(angle);
+            if (i == -1) {
+                generator.addPoint(x, 0, z, 0, -1, 0);
+                offset++;
+                continue;
+            }
 
             float slantLength = sqrt(radius * radius + height * height);
             float n1 = cos(angle) * height / slantLength;
@@ -28,9 +33,9 @@ void Cone::createCone(float radius, float height, int slices, int stacks, const 
 
             generator.addPoint(x, y, z, n1, n2, n3);
 
-            int currPointIndex = i * slices + (j + 1);
-            int neighbourPointIndex = (currPointIndex + 1) % (slices * (i + 1) + 1);
-            neighbourPointIndex += neighbourPointIndex == 0 ? 1 + slices * i : 0;
+            int currPointIndex = i * slices + (j + 1) + offset;
+            int neighbourPointIndex = (currPointIndex + 1) % (slices * (i + 1) + 1 + offset);
+            neighbourPointIndex += neighbourPointIndex == 0 ? 1 + slices * i + offset : 0;
 
             int p1 = currPointIndex;
             int p2 = neighbourPointIndex;
@@ -52,7 +57,7 @@ void Cone::createCone(float radius, float height, int slices, int stacks, const 
     // if we have 9 slices, i.e., 9 points, we need to
     // have N associations
     for (int j = 0; j < slices; j++) {
-        int p1 = j + 1; // an index, for now, is never 0
+        int p1 = j + 1;
         int p2 = (j + 2) % (slices + 1); // look for the neighbor point, but ensure it doesn't overflow
         p2 += p2 == 0 ? 1 : 0; // if a points overflows, the result will be the index 0, which, for now, cannot happen
         generator.addAssociation(p1, p2, baseCenterIndex);
