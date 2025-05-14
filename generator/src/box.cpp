d01b6c368fb42aa463b3dd1b0c3a175991246913
dudu <eduardo21faria@gmail.com>
2025-05-14 16:51:43 +0100

    feat: was going to add rectangular skybox, ended up not needing it; texture coordinates for box

--- generator/src/box.cpp
+++ generator/src/box.cpp
@@ -12,6 +12,9 @@ void Box::createBox(int dimension, int divPerEdge, const std::string& filename)
     // i.e., 3 divisions means 4 points
     const int pointsPerEdge = divPerEdge + 1;
 
+    // half‐size of the box
+    const float half = dimension / 2.0f;
+
     // iterate over the 6 possible normals' values
     // (u, v, w)
     // (0, 0, 1)
@@ -55,7 +58,27 @@ void Box::createBox(int dimension, int divPerEdge, const std::string& filename)
                 const float y = isYNormal ? dy : maxWidth - 2.0f * (i * isXNormal + j * isZNormal) / divPerEdge;
                 const float z = isZNormal ? dz : maxWidth - 2.0f * j / divPerEdge;
 
-                pointGen.addPoint(x, y, z, u, v, w, 0, 0);
+                float s, t;
+                float xNormalized = (static_cast<float>(i) / divPerEdge) * 0.25;
+                float yNormalized = (static_cast<float>(j) / divPerEdge) * 0.33;
+                if (w != 0) {
+                    s = xNormalized;
+                    s += w == -1 ? 0.5 : 0;
+
+                    t = yNormalized + 0.33;
+                } else if (v != 0) {
+                    s = xNormalized + 0.25;
+
+                    t = yNormalized;
+                    t += v == -1 ? 0 : 0.66;
+                } else if (u != 0) {
+                    s = xNormalized;
+                    s += u == -1 ? 0.25 : 0.75;
+
+                    t = yNormalized + 0.33;
+                }
+
+                pointGen.addPoint(x, y, z, u, v, w, s, t);
 
                 int currPointIndex = k * pointsPerEdge * pointsPerEdge + i * pointsPerEdge + j + 1;
 
